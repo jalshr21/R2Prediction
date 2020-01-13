7e55d0c48a928a277be674bbd738ea02fc16ceaa
jalshr21 <shrutijalan@Shrutis-MacBook-Pro.local>
2020-01-13 08:49:14 +0530

    Read data

---
+++ src/data/reading.py
@@ -0,0 +1,30 @@
+import sqlite3
+from os import path
+import pandas as pd
+
+
+def readSqliteData(filepath):
+    if path.isfile(filepath):
+        dbcon = sqlite3.connect(filepath)
+    else:
+        print(" file does not exist!")
+        exit(-1)
+    table = "data"
+    query = "pragma table_info(" + table + ")"
+    cursor = dbcon.cursor()
+    cursor.execute(query)
+    row = cursor.fetchone()
+
+    query = "select * from " + table + ";"
+    cursor = dbcon.cursor()
+    cursor.execute(query)
+
+    data = []
+    row = cursor.fetchone()
+    while row:
+        data.append((row[1], row[3]))
+        row = cursor.fetchone()
+    dfObj = pd.DataFrame(data, columns=["text", "label"])
+    cursor.close()
+    dbcon.close()
+    return dfObj
