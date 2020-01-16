80bb7318f61b3f17ed830788e12573c873bacea9
jalshr21 <shrutijalan@Shrutis-MacBook-Pro.local>
2020-01-16 09:45:11 +0530

    Add Swagger

--- app.py
+++ app.py
@@ -1,28 +1,39 @@
 from flask import request, Flask
+from flask_restplus import Api, Resource
 from src.scripts import predict
 from src.scripts import train
 
 
-app = Flask(__name__)
+flask_app = Flask(__name__)
+app = Api(app=flask_app)
+name_space = app.namespace('main', description='Main APIs')
 
-@app.route('/get-prediction')
-def query_example():
-    language = request.args.get('text')
-    ans = predict.predict([language])
-    if ans[0] == 0:
-        return "Not Relevant"
-    elif ans[0] == 1:
-        return "Relevant"
-    else:
-        return "Can't Say"
+@name_space.route("/get-prediction")
+class MainClass(Resource):
+    def get(self):
+        language = request.args.get('text')
+        ans = predict.predict([language])
+        if ans[0] == 0:
+            return "Not Relevant"
+        elif ans[0] == 1:
+            return "Relevant"
+        else:
+            return "Can't Say"
 
-@app.route('/train-model')
-def query_example_1():
-    train.fit()
-    return "Model trained"
+@name_space.route("/testSwagger")
+class MainClass(Resource):
+	def get(self):
+		return {
+			"status": "Got new data"
+		}
+	def post(self):
+		return {
+			"status": "Posted new data"
+		}
+
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    flask_app.run(debug=True, host='0.0.0.0')
 
 
